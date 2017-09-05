5993956604579fadd69f3e03d6d5a0ab47e9b09d
Jason <31069084+charadox@users.noreply.github.com>
2017-09-05 08:19:26 -0400

    did some stuff

--- aya.py
+++ aya.py
@@ -23,6 +23,10 @@
 ***REMOVED***
 ***REMOVED***
 
+***REMOVED***
+***REMOVED***
+    """To show the names of the cogs"""
+    await Aya.say('`help`, `invite`, `moderator`, `coinflip`, `dice`, `gif`, `8ball`, `userfeatures`, `minigames`')
 
 ***REMOVED***
 ***REMOVED***
--- cogs/userfeatures.py
+++ cogs/userfeatures.py
@@ -5,8 +5,7 @@ from datetime import datetime
 import time
 
 default_settings = {'PAYDAY_TIME': 86400, 'PAYDAY_AMOUNT': 1000, 'REGISTER_AMOUNT': 500}
-account = [False, False, False***REMOVED***
-
+account = [False, False***REMOVED***
 class UserFeatures:
 
     def __init__(self, Aya):
@@ -17,16 +16,19 @@ class UserFeatures:
     async def register(self, ctx):
         author = ctx.message.author
         '''Register a bank account'''
-        if account == False:
+        if account[0***REMOVED*** == False:
+            global balance
             balance = 0
-            account = True
+            account[0***REMOVED*** = True
             await self.Aya.say('Registration complete')
 ***REMOVED***
-            await self.Aya.say('{}You already have an account.'.format(author.mention))
+            await self.Aya.say('{} You already have an account.'.format(author.mention))
+
+    @commands.command(pass_context=True)
     async def payday(self, ctx):
         '''Gives you money every 24 hours'''
         author = ctx.message.author
-        if account == True:
+        if account[0***REMOVED*** == True:
             balance += 1000
             await self.Aya.say('{} You earned ${}. Wait 24 hours for your next payday.'.format(author.mention, default_settings['PAYDAY_AMOUNT'***REMOVED***))
 ***REMOVED***
@@ -34,11 +36,13 @@ class UserFeatures:
 
     @commands.command(pass_context=True)
     async def balance(self, ctx):
+        '''Check your account's balance'''
         author = ctx.message.author
-        if account == True:
+        if account[0***REMOVED*** == True:
             await self.Aya.say('{} Your balance is ' + balance + '.'.format(author.mention))
 ***REMOVED***
             await self.Aya.say('{} You don\'t have an account yet. Type `a.register` to create an account.'.format(author.mention))
+
 
 
 def setup(Aya):
