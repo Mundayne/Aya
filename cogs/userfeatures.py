1fb93ee217b5ef036845560ba8079651ffbcd64b
Jason <31069084+charadox@users.noreply.github.com>
2017-09-10 18:22:18 -0400

    made balance and register work FINALLLY

--- cogs/userfeatures.py
+++ cogs/userfeatures.py
@@ -6,29 +6,10 @@ import time
 import os
 import json
 
-default_settings = {'PAYDAY_TIME': 86400, 'PAYDAY_AMOUNT': 1000, 'REGISTER_AMOUNT': 500}
-account = [False, False***REMOVED***
-class UserFeatures:
 
+class UserFeatures:
     def __init__(self, Aya):
         self.Aya = Aya
-
-    def register(self, ctx):
-        #read the data from the file and convert it to json
-        with open('bankholders.json', 'r') as f:
-            data = json.loads(f.read())
-
-        user_id = ctx.message.author.id
-        guild_id = ctx.message.server.id
-
-        #if not in data, give money to start off
-        if user_id not in data:
-            user = {'guild': guild_id, 'money': 100}
-            data[user_id***REMOVED*** #add that data to the database
-
-        #save that data back to the database
-        with open('bankholders.json', 'w') as f:
-            f.write(json.dumps(data))
 
 
     @commands.command(pass_context=True)
@@ -36,22 +17,34 @@ class UserFeatures:
         # read the data from the file and convert it to json
         with open('data/bankholders.json', 'r') as f:
             data = json.loads(f.read())
-
-        user_id = ctx.message.author.id
+        # get the IDs
+        user_id = str(ctx.message.author.id)
         guild_id = ctx.message.server.id
 
         # if not in data, give money to start off
         if user_id not in data:
-            user_account = {'user': user_id, 'guild': guild_id, 'money': 1000}
-            await self.Aya.say('Registration complete. Balance: $1000')
+            user = {'user': user_id, 'guild': guild_id, 'money': 200}
+            data[user_id***REMOVED*** = user
+            await self.Aya.say('Registration complete. Balance: $200')
 ***REMOVED***
             await self.Aya.say('You already have an account.')
 
         # save that data back to the database
-        with open('bankholders.json', 'w') as f:
-            f.write(json.dumps(data))
+        with open('data/bankholders.json', 'w') as f:
+            f.write(json.dumps(data, indent=4))
 
-
+    @commands.command(pass_context=True, aliases=['bal'***REMOVED***)
+    async def balance(self, ctx):
+        user_id = str(ctx.message.author.id)
+        with open('data/bankholders.json', 'r') as f:
+            data = json.loads(f.read())
+        em = discord.Embed(title='Balance', color=0x2ECC71)
+        em.add_field(name='Account Holder', value=ctx.message.author)
+        em.add_field(name='Account Balance', value=data[user_id***REMOVED***['money'***REMOVED***)
+***REMOVED***
+            await self.Aya.say(embed=em)
+        except discord.HTTPException:
+            await self.Aya.say('{} I need the embed links permission to send this.'.format(serv_owner.mention))
 
 
 def setup(Aya):
