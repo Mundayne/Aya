78931ed7a4db08b5a1923769128b80d597f7c23a
Oxilium <24987408+TheOxilium@users.noreply.github.com>
2017-09-11 20:40:11 +0100

    Update Userfeatures

--- cogs/userfeatures.py
+++ cogs/userfeatures.py
@@ -9,14 +9,14 @@ class UserFeatures:
         self.Aya = Aya
 
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def register(self, ctx):
         # read the data from the file and convert it to json
         with open('data/bankholders.json', 'r') as f:
             data = json.loads(f.read())
         # get the IDs
         user_id = str(ctx.message.author.id)
-        guild_id = ctx.message.server.id
+        guild_id = discord.Guild.id
 
         # if not in data, give money to start off
         if user_id not in data:
@@ -24,28 +24,28 @@ class UserFeatures:
             data[user_id] = user
             global pdcollect
             pdcollect = False
-            await self.Aya.say('Registration complete. Balance: $200')
+            await ctx.send('Registration complete. Balance: $200')
         else:
-            await self.Aya.say('You already have an account.')
+            await ctx.send('You already have an account.')
 
         # save that data back to the database
         with open('data/bankholders.json', 'w') as f:
             f.write(json.dumps(data, indent=4))
 
-    @commands.command(pass_context=True, aliases=['bal'])
+    @commands.command(aliases=['bal'])
     async def balance(self, ctx):
         user_id = str(ctx.message.author.id)
         with open('data/bankholders.json', 'r') as f:
             data = json.loads(f.read())
         em = discord.Embed(title='Balance', color=0x2ECC71)
-        em.add_field(name='Account Holder', value=ctx.message.author)
+        em.add_field(name='Account Holder', value=discord.Message.author)
         em.add_field(name='Account Balance', value=data[user_id]['money'])
         try:
-            await self.Aya.say(embed=em)
+            await ctx.send(embed=em)
         except discord.HTTPException:
-            await self.Aya.say('{} I need the embed links permission to send this.'.format(serv_owner.mention))
+            await ctx.send('{} I need the embed links permission to send this.'.format(serv_owner))
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def payday(self, ctx):
         user_id = str(ctx.message.author.id)
         with open('data/bankholders.json', 'r') as f:
