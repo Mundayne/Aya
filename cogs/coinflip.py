2660367e15e3cf6bf79680657da2212ffdf343c7
Oxilium <24987408+TheOxilium@users.noreply.github.com>
2017-09-11 20:23:24 +0100

    Update coinflip

--- cogs/coinflip.py
+++ cogs/coinflip.py
@@ -9,19 +9,19 @@ class Flip:
     def __init__(self, Aya):
         self.Aya = Aya
 
-    @commands.command(pass_context=True, aliases=['flipcoin'])
-    async def coinflip(self):
+    @commands.command(aliases=['flipcoin'])
+    async def coinflip(self, ctx):
         """Flips a coin"""
-        serv_owner = ctx.message.server.owner
+        serv_owner = discord.Guild.owner
         choices = ['You got Heads', 'You got Tails']
         color = ('#%06x' % random.randint(8, 0xFFFFFF))
         color = int(color[1:], 16)
         color = discord.Color(value=color)
         em = discord.Embed(color=color, title='Coinflip:', description=random.choice(choices))
         try:
-            await self.Aya.say(embed=em)
+            await ctx.send(embed=em)
         except discord.HTTPException:
-            await self.Aya.say('{} I need the embed links permission to send this.'.format(serv_owner.mention))
+            await ctx.send('{} I need the embed links permission to send this.'.format(serv_owner.mention))
 
 
 def setup(Aya):
