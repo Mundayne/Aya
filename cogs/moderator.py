e571d2b06d4c38c78c9dded10eb04249fe9740ef
Oxilium <24987408+TheOxilium@users.noreply.github.com>
2017-09-11 20:36:54 +0100

    Update moderator cog

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -9,44 +9,44 @@ class Mod:
     def __init__(self, Aya):
         self.Aya = Aya
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def kick(self, ctx, *, member):
         """Kicks someone out of the server"""
         kickmsg = ['Poor %s is out!', 'These people don\'t understand!', 'Seeya %s!']
         try:
-            await self.Aya.kick(member)
-            await self.Aya.say(random.choice(kickmsg) % member)
+            await ctx.kick(member)
+            await ctx.send(random.choice(kickmsg) % member)
         except discord.Forbidden:
-            await self.Aya.send_message(ctx.message.channel, '{}: Who do you think you are?'
-                                        .format(ctx.message.author))
+            await ctx.send(channel, '{}: Who do you think you are?'
+                                        .format(discord.Message.author))
 
     @commands.command(pass_context=True)
     async def ban(self, ctx, *, member):
         banmsg = ['We won\'t see %s again!', 'These people really really don\'t understand', 'See you never %s!']
         try:
-            await self.Aya.ban(member, delete_message_days=1)
-            await self.Aya.say(random.choice(banmsg) % member)
+            await ctx.ban(member, delete_message_days=1)
+            await ctx.send(random.choice(banmsg) % member)
         except discord.Forbidden:
-            await self.Aya.send_message(ctx.message.channel, '{}: Who do you really think you are?'
-                                        .format(ctx.message.author))
+            await ctx.send(channel, '{}: Who do you really think you are?'
+                                        .format(discord.Message.author)
 
     @commands.command(pass_context=True)
     async def unban(self, ctx, *, member):
         unbanmsg = ['%s is back from hell!', 'Welcome back! %s']
         try:
-            await self.Aya.unban(discord.Server, member)
-            await self.Aya.say(random.choice(unbanmsg) % member)
+            await ctx.unban(discord.Server, member)
+            await ctx.send(random.choice(unbanmsg) % member)
         except discord.Forbidden:
-            await self.Aya.send_message(ctx.message.channel, '{}: You don\'t understand how this place works, do you?'
-                                        .format(ctx.message.author))
+            await ctx.send(channel, '{}: You don\'t understand how this place works, do you?'
+                                        .format(discord.Message.author))
 
     @commands.command(pass_context=True)
     async def getbans(self, ctx, *, server=discord.Server):
         try:
             for x in server:
-                await self.Aya.say(str(x))
+                await ctx.send(str(x))
         except discord.Forbidden:
-            await self.Aya.send_message(ctx.message.channel, '{} : You don\'t understand how this place works, do you?'
-                                        .format(ctx.message.author))
+            await ctx.send(channel, '{} : You don\'t understand how this place works, do you?'
+                                        .format(discord.Message.author))
 def setup(Aya):
     Aya.add_cog(Mod(Aya))
