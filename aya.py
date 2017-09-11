092236f135815828e0dd111093ffc75abfba488a
Oxilium <24987408+TheOxilium@users.noreply.github.com>
2017-09-11 11:03:39 +0100

    Edit cogs command

--- aya.py
+++ aya.py
@@ -26,8 +26,10 @@
 
 ***REMOVED***
 ***REMOVED***
-    """To show the names of the cogs"""
-    await Aya.say('`help`, `invite`, `moderator`, `coinflip`, `dice`, `gif`, `8ball`, `userfeatures`, `minigames`')
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
