6f2262506af4113c5f8ed04eff3a9496c496db42
Oxilium <24987408+TheOxilium@users.noreply.github.com>
2017-09-11 20:24:48 +0100

    Update GIF

--- cogs/gif.py
+++ cogs/gif.py
@@ -10,10 +10,10 @@ class Gif:
     def __init__(self, Aya):
         self.Aya = Aya
 
-    @commands.command(pass_context=True)
+    @commands.command(e)
     async def gif(self, ctx, *, tag):
         ''' Get a random gif. Usage: gif <tag> '''
-        serv_owner = ctx.message.server.owner
+        serv_owner = discord.Guild.owner
         g = safygiphy.Giphy()
         gif = g.random(tag=tag)
         color = ("#%06x" % random.randint(8, 0xFFFFFF))
@@ -22,9 +22,9 @@ class Gif:
         em = discord.Embed(color=color)
         em.set_image(url=str(gif.get('data', {}).get('image_original_url')))
         try:
-            await self.Aya.say(embed=em)
+            await ctx.send(embed=em)
         except discord.HTTPException:
-            await self.Aya.say('{} I need the embed links permission to send this.'.format(serv_owner.mention))
+            await ctx.send('{} I need the embed links permission to send this.'.format(serv_owner.mention))
 
 
 def setup(Aya):
